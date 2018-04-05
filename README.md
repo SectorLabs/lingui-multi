15a8fb32777fc3304412b1c18cef1a69dfc69efe
Ahmed Belal <ab@zameen.com>
2018-04-05 22:33:42 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Lingui Multi
+This is a command line utility for generating multiple catalog files from a single (unified) messages JSON file using js-lingui. Please ensure your application uses js-lingui before attempting to use this utility.
+
+# Setup
+Add the following to your project's package.json file:
+```json
+ "lingui-multi": {
+    "sub_catalog_name": {
+        "srcPathIgnorePatterns": [
+            "<ignore_folder>",
+            "<ignore_folder_2>"
+            ...
+        ]
+    },
+    "sub_catalog_2_name": {
+        "srcPathIgnorePatterns": [
+            "<ignore_folder_3>",
+            "<ignore_folder_4>"
+            ...
+        ]
+    }
+ }
+```
+
+Each sub-catalog will include all the i18n translations used in the source paths defined for lingui in the package.json file minus the ignore patterns defined either in the lingui configuration or the lingui-multi sub-catalog configuration.
+
+# Usage
+You can run the utility by executing the command provided by the package like so:
+```shell
+lingui-multi <path-to-application-package.json> <path-to-locale-directory>
+```
+
+The utility will pick up lingui configuration and lingui-multi configuration from the package.json file and spit out catalog files in the locale directory with the format `<sub_catalog>.messages.js` for each sub-catalog.
+
---
+++ dist/lingui-multi.js
@@ -0,0 +1,161 @@
+#! /usr/bin/env node
+
+const compile = require('@lingui/cli/api/compile');
+const extract = require('@lingui/cli/api/extract');
+const commander = require('commander');
+const rimraf = require('rimraf');
+
+const _ = require('lodash');
+
+const path = require('path');
+const util = require('util');
+const fs = require('fs');
+
+commander.version(require("../package.json").version).arguments("[package-json]  [locale-dir]"
+).action(function (packageJson, localeDirectory)
+{
+    package = packageJson;
+    localeDir = localeDirectory;
+}).parse(process.argv);
+
+
+
+if (typeof package === 'undefined')
+{
+    console.info('no package.json path supplied, using default: ./package.json');
+    package = './package.json';
+}
+
+console.info('Package json: ' + path.resolve(package));
+
+if (fs.existsSync(package) === false)
+{
+    console.error('ERROR: package.json does not exist');
+    process.exit(1);
+}
+
+packageObject = JSON.parse(fs.readFileSync(package));
+
+if (typeof localeDir === 'undefined')
+{
+    console.info('no locale directory path supplied, using default: ./locale');
+    localeDir = './locale';
+}
+
+console.info('Locale directory: ' + path.resolve(localeDir));
+
+if (fs.existsSync(localeDir) === false)
+{
+    console.error('ERROR: locale directory does not exist');
+    process.exit(1);
+}
+
+locales = fs.readdirSync(localeDir);
+
+if (!('lingui' in packageObject))
+{
+    console.error('ERROR: No lingui config found');
+    process.exit(1);
+}
+
+if (!('lingui-multi' in packageObject))
+{
+    console.error('ERROR: No lingui-multi bundles config found');
+    process.exit(1);
+}
+
+
+// The directory where we are going to do the extract/collect
+const targetDir = './lingui-multi';
+console.info("Creating temporary build directory");
+
+// Create target dir if not exist
+if (fs.existsSync(targetDir) === false)
+    fs.mkdirSync(targetDir);
+
+
+let buildDir = targetDir + '/_build';
+
+// Create build dir if not exist
+if (fs.existsSync(buildDir) === false)
+    fs.mkdirSync(buildDir);
+
+console.info('Build scratchpad directory: ' + path.resolve(buildDir));
+
+// Iterate the language bundles
+for (bundle in packageObject['lingui-multi'])
+{
+    console.info(util.format("Building %s language bundle", bundle));
+    // Remove build dir contents on each run
+    rimraf.sync(buildDir + '/*');
+
+    let options = Object.assign({}, packageObject.lingui);
+
+    let srcPathDirs = packageObject.lingui.srcPathDirs;
+
+    // Dirty patch for <rootDir>
+    options.srcPathDirs = [];
+
+    srcPathDirs.forEach(function (dir)
+    {
+        options.srcPathDirs.push(dir.replace('<rootDir>', path.dirname(package)));
+    });
+
+
+    // Convert from CLI to API keys
+    if ('srcPathIgnorePatterns' in options)
+    {
+        options.ignore = options.srcPathIgnorePatterns;
+    }
+
+    if ('srcPathIgnorePatterns' in packageObject['lingui-multi'][bundle])
+    {
+        options.ignore = (options.ignore || []).concat(packageObject['lingui-multi'][bundle]['srcPathIgnorePatterns']);
+    }
+
+    delete options.srcPathIgnorePatterns;
+
+    extract.extract(options.srcPathDirs, targetDir, options);
+
+    let catalogObject = extract.collect(buildDir);
+
+    let keys = Object.keys(catalogObject);
+
+    // Go over each locale
+    locales.forEach(function (locale)
+    {
+        let messagesObject = {};
+
+        // Only continue if locale is a directory
+        if (fs.lstatSync(path.resolve(localeDir, locale)).isDirectory() === false)
+        {
+            return;
+        }
+
+        filePath = util.format('%s/%s/messages.json', localeDir, locale);
+        if (fs.existsSync(filePath) === false)
+        {
+            console.info(util.format('INFO: File not found for conversion: %s', filePath));
+            return;
+        }
+
+        messagesObject = Object.assign(messagesObject, JSON.parse(fs.readFileSync(filePath)));
+
+        screenedMessages = _.pick(messagesObject, keys);
+
+        jsData = compile.createCompiledCatalog(locale, screenedMessages);
+
+        targetFile = util.format('%s/%s/%s.js', localeDir, locale, bundle);
+
+        fs.writeFileSync(targetFile, jsData);
+
+        console.info(util.format('Wrote %d messages to %s/%s.messages.js', Object.keys(screenedMessages).length, locale, bundle));
+    });
+
+}
+
+// Cleanup
+console.info("Removing temporary build directory");
+rimraf.sync(targetDir);
+console.info("Done");
+
